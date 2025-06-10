2470f92e65de45a16ed19d0b29e38f787800894a
malvis Anochie <malvisanochie97@gmail.com>
2025-06-10 22:37:46 +0100

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+import { ModeToggle } from "@/components/dark-theme-toggle";
+import { div } from "framer-motion/client";
+export default function Home() {
+  return (
+   <div>
+    {/* Navbar */}
+    <section>
+      <div className="flex items-center justify-between p-4">
+<div>
+
+</div>
+    </div>
+    </section>
+    
+    {/* End of Navber */}
+   </div>
+  );
+}
