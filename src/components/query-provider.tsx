99170539705bfb44806b1e1ca88167c850663601
malvis Anochie <malvisanochie97@gmail.com>
2025-10-09 11:52:14 +0100

    updated

--- src/components/query-provider.tsx
+++ src/components/query-provider.tsx
@@ -3,7 +3,11 @@
 import React from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-export function ReactQueryProvider({ children }: { children: React.ReactNode }) {
+export function ReactQueryProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   // create the client once per session on the client
   const [client] = React.useState(() => new QueryClient());
 
